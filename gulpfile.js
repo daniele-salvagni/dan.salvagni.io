9a4e32aa348f7d7f402fd21e8ed86c91f3916707
Daniele Salvagni <daniele.salvagni@gmail.com>
2017-03-21 10:32:39 +0100

    Fix bugs and add Server task

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,57 @@
-// Dependencies ==========================================================================
+// Dependencies --------------------------------------------------------------------------
 
 // Common
-var gulp = require('gulp');
-var path = require('path');
-var argv = require('minimist')(process.argv.slice(2));
+let argv = require('minimist')(process.argv.slice(2));
+let path = require('path');
+let gulp = require('gulp');
 
 // Metalsmith
-var Metalsmith = require('metalsmith'); // Plugins are included from config.js
+let Metalsmith = require('metalsmith'); // Plugins are included from config.js
 
 // Configuration
-var config = require('./config');
+let config = require('./config');
 
 // Sass
-var sass      = require('gulp-sass');
-var normalize = require('node-normalize-scss'),
+let sass      = require('gulp-sass');
+let normalize = require('node-normalize-scss'),
     bourbon   = require('bourbon'),
     neat      = require('bourbon-neat');
 
 // Template
-var handlebars = require('handlebars'),
+let handlebars = require('handlebars'),
     layouts = require('handlebars-layouts');
 handlebars.registerHelper(layouts(handlebars));
 
 // Configuration
-var args = {
+let args = {
   build: !!argv.build,
   production: !!argv.production
 };
 
 
-// Metalsmith task =======================================================================
+// Metalsmith task -----------------------------------------------------------------------
 
 function setupMetalsmith(callback) {
-  var ms = new Metalsmith(process.cwd());
-  var msconfig = config.metalsmith || {};
-  var msplugins = msconfig.plugins || {};
+  let ms = new Metalsmith(process.cwd());
+  let msconfig = config.metalsmith || {};
+  let msplugins = msconfig.plugins || {};
 
   ms.source(msconfig.config.contentRoot);
   ms.destination(msconfig.config.destRoot);
   ms.metadata(msconfig.metadata);
 
+  // Load all plugins with the respective configuration
   Object.keys(msplugins).forEach(function(key) {
-    var plugin = require(key);
-    var options = msplugins[key];
+    let plugin = require(key);
+    let options = msplugins[key];
 
     // Some plugins can be enabled only in production by using:
-    // "_metalsmith_if": "production",
+    // '_metalsmith_if': 'production',
     if (options._metalsmith_if !== undefined) {
-      var condition = false;
-      if (options._metalsmith_if === "production") {
+      let condition = false;
+      if (options._metalsmith_if === 'production') {
         condition = argv.production;
-      } else if (options._metalsmith_if === "build") {
+      } else if (options._metalsmith_if === 'build') {
         condition = argv.build;
       }
 
@@ -60,6 +61,7 @@ function setupMetalsmith(callback) {
         ms.use(plugin(options));
       }
     } else {
+      // '_metalsmith_if' is not set
       ms.use(plugin(options));
     }
   });
@@ -69,7 +71,6 @@ function setupMetalsmith(callback) {
       console.log(err);
       return callback(err);
     }
-
     callback();
   });
 };
@@ -79,10 +80,10 @@ gulp.task('metalsmith', function(callback) {
 });
 
 
-// Styles task ===========================================================================
+// Styles task ---------------------------------------------------------------------------
 
 gulp.task('styles', function() {
-  return gulp.src(path.join(__dirname, config.metalsmith.config.styleRoot, 'app.scss'))
+  return gulp.src(path.join(__dirname, config.metalsmith.config.styleRoot, 'main.scss'))
     .pipe(sass({
       sourceComments: args.production ? false : true,
       outputStyle: args.production ? 'compressed' : 'expanded',
@@ -98,10 +99,39 @@ gulp.task('styles', function() {
     //  browsers: config.styles.prefix,
     //  cascade: false
     //}))
-    .pipe(gulp.dest(path.join(__dirname, config.metalsmith.config.assetRoot, 'assets')));
+    .pipe(gulp.dest(path.join(__dirname, config.metalsmith.config.destRoot, 'assets')));
 });
 
 
-// Default task ==========================================================================
+// Default task --------------------------------------------------------------------------
 
 gulp.task('default', ['styles', 'metalsmith']);
+
+
+// Server task ---------------------------------------------------------------------------
+
+gulp.task('server', ['default'], function(callback) {
+  let http = require('http');
+  let serveStatic = require('serve-static');
+  let finalhandler = require('finalhandler');
+
+  let serve = serveStatic(config.metalsmith.config.destRoot, {
+    "index": ['index.html', 'index.htm'],
+    "maxAge": 0
+  });
+
+  let server = http.createServer(function(req, res){
+    let done = finalhandler(req, res);
+    serve(req, res, done);
+  })
+
+  let serverPort = 5555;
+  if (argv.port) {
+    serverPort = parseInt(argv.port);
+  }
+
+  server.listen(serverPort, function() {
+    console.log('Server: http://localhost:%s', serverPort);
+    callback();
+  });
+});
